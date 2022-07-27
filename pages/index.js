5e3d04265bbab36f2a8ac688b08300054d689bf0
Lucy de Rojas <lucyderojas@gmail.com>
2022-07-27 12:13:03 +0100

    initiated nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Skillex</title>
-        <meta name="description" content="Skill Ex " />
-        <link rel="icon" href="/LightBulbFAVICON.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Skillex</title>
+				<meta name='description' content='Skill Ex ' />
+				<link rel='icon' href='/LightBulbFAVICON.ico' />
+			</Head>
 
-   
-   <h1>Skill Ex</h1>
-  
-
-
-
-    </div>
-  )
+			<h1>Skill Ex</h1>
+		</div>
+	);
 }
