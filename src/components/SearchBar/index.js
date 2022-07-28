00c467b7ddb1bd13c36786df5a65f0e4fe5975e4
Jay Whittingham <jmlwhittingham@gmail.com>
2022-07-28 16:14:55 +0100

    finished search bar but still need logic

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,11 +1,26 @@
 //SearchBar
 import styles from "./searchbar.module.css"
+import { useState } from "react"
+
+const Searchbar = () => {
+    const [text, setText] = useState ("")
 
 
-const Searchbar = ({ text }) => {
+    const handleChange = (event) => {
+        setText (event.target.value)
+        console.log( text )
+        
+    }
+
+    const handleClick = () => {
+        console.log("submitted", text) /* include search logic (get request etc)*/
+        setText ("")
+    }
+    
+    
     return ( <div className = {styles.container}>
-        <input className = {styles.searchInput} placeholder="Search here"/> 
-        <img className = {styles.magnifyingGlass} src="/media/icons/searchIconWHITE.png" alt="Search"/>
+        <input value={text} onChange={handleChange} className = {styles.searchInput} placeholder="Search here"/> 
+        <img onClick={handleClick} className = {styles.magnifyingGlass} src="/media/icons/searchIconWHITE.png" alt="Search"/>
     </div> );
 }
  
