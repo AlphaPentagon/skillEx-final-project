e5248e7a620b3478dc94e94128b254b47ee86821
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-07-28 11:50:07 +0100

    Created and styled button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,18 +1,23 @@
-//Button
-// create a button.module.css file
-// import the css module into the js file
+import styles from "./button.module.css";
 
-// create a react component
-// props: text ? type (bg colour) ?
-// button element
-// export the component
+const Button = ({ text, colour }) => {
+  return (
+    <button className={`${styles[colour]} ${styles.default}`}>{text}</button>
+  );
+};
+
+export default Button;
+
+//Button
+// create a button.module.css file ✅
+// import the css module into the js file ✅
+
+// create a react component ✅
+// props: text ? type (bg colour) ? ✅
+// button element ✅
+// export the component ✅
 
 // css
-// a class for each bgcolour e.g. blue-button, brown-button
+// a class for each bgcolour e.g. blue-button, brown-button ✅
 /*
-
-<App>
-    <button>Submit</button>
-</App>
-
-*/
+ */
