530725749e785216b1a16501d93c5b228d40cd5a
Lucy de Rojas <lucyderojas@gmail.com>
2022-07-28 11:49:14 +0100

    navigation initaited

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,13 +1,35 @@
 //Logo
 import Image from 'next/image'
+import styles from '../Logo/logo.module.css';
+
 
 export default function Logo() {
 
 
 
-    return (<div>
+    return (<div className={styles.logoContainer}>
+
+<div className={styles.logoText}>
+
         <p className="logo">SkillEx</p>
         <p className='tagLine'>The Knowhow Exchange</p>
+
+</div>
+
+
+
+<div className={styles.logoImage}>
+
+        <Image 
+            src='/media/icons/LightBulbFAVICON.svg'
+            // src='../../../public/media/icons/LightBulbFAVICON.svg'
+            width={100}
+            height={100}
+            
+            
+            
+            />
+            </div>
         
 
 
