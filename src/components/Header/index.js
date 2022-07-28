b8acd5fd5023c3234b86e7bfc7c373dba1291290
Jay Whittingham <jmlwhittingham@gmail.com>
2022-07-28 12:06:47 +0100

    creating header component and css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,17 @@
 //Header
+
+const Header = ({ text, colour }) => {
+    return ( <div className={styles[colour]}><h1>{text}</h1></div> );
+}
+ 
+export default Header;
+
+
+/* 
+create header.module.css 
+import and export 
+create a component (possible h1) to store text
+taking in same terraCotta colour
+
+
+*/
