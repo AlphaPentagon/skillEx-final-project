73f88224f09843112b7d34fad4fcb97d92d1df6f
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-07-28 14:06:13 +0100

    Created menu with visibility toggle, page links and log-in status links with state. CSS styling also included

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,14 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>SkillEx</title>
-        <meta name="description" content="SkillEx " />
-        <link rel="icon" href="/LightBulbFAVICON.ico" />
-      </Head>
-
-      <h1>SkillEx</h1>
-      <p>The knowhow exchange</p>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>SkillEx</title>
+				<meta name="description" content="SkillEx " />
+				<link rel="icon" href="/LightBulbFAVICON.ico" />
+			</Head>
+			<Menu />
+		</div>
+	);
 }
