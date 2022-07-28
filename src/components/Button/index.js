89c0c814ace822f27f93acac2367a3171eed0326
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-07-28 10:37:32 +0100

    Created plan

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,1 +1,18 @@
 //Button
+// create a button.module.css file
+// import the css module into the js file
+
+// create a react component
+// props: text ? type (bg colour) ?
+// button element
+// export the component
+
+// css
+// a class for each bgcolour e.g. blue-button, brown-button
+/*
+
+<App>
+    <button>Submit</button>
+</App>
+
+*/
