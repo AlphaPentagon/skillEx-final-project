672f3f0fcbf024baba4988831f097a9a99246d52
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-07-29 14:23:10 +0100

    Created layout component, imported it into index.js

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,32 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Navigation from "../src/components/Navigation";
+import Layout from "../src/components/Layout/Layout";
 import Header from "../src/components/Header";
 import SearchBar from "../src/components/SearchBar";
 import Testimonials from "../src/components/Testimonials";
 import WhyChooseUs from "../src/components/WhyChooseUs";
 import AboutUs from "../src/components/AboutUs";
-import Footer from "../src/components/Footer";
 import SkillGallery from "../src/components/SkillGallery";
+
 export default function Home() {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>SkillEx</title>
-				<meta
-					name="SkillEx - The knowhow exchange where people swap skills."
-					content="SkillEx "
-				/>
-				<link rel="icon" href="/LightBulbFAVICON.ico" />
-			</Head>
-			<Navigation />
-			<Header />
-			<SearchBar />
-			<AboutUs />
-			<WhyChooseUs />
-			<Testimonials />
-			<SkillGallery />
-			<Footer />
-		</div>
-	);
+  return (
+    <>
+      <Head>
+        <title>SkillEx</title>
+        <meta
+          name="SkillEx - The knowhow exchange where people swap skills."
+          content="SkillEx "
+        />
+        <link rel="icon" href="/LightBulbFAVICON.ico" />
+      </Head>
+      <Layout>
+        <Header />
+        <SearchBar />
+        <AboutUs />
+        <WhyChooseUs />
+        <Testimonials />
+        <SkillGallery />
+      </Layout>
+    </>
+  );
 }
