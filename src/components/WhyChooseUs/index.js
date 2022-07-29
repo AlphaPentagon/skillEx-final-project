071089c83ea2acd241e185ba3df882c8f3df1cb7
ZainabAbid1 <103643063+ZainabAbid1@users.noreply.github.com>
2022-07-29 10:48:30 +0100

    feature card showing up

--- src/components/WhyChooseUs/index.js
+++ src/components/WhyChooseUs/index.js
@@ -1,1 +1,22 @@
 //Why Choose Us
+import { FeatureData } from "../../libs/whyChooseUs";
+import FeatureCard from "../featureCard";
+
+
+const WhyChooseUs = () => {
+  return (
+<div>
+      {FeatureData.map((data) => (
+        <FeatureCard 
+
+        key={data.feature_id} 
+        icon={data.icon} 
+        desc={data.desc} 
+
+        />
+      ))}
+   </div> 
+  );
+};
+
+export default WhyChooseUs;
