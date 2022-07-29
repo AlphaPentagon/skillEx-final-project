d3d5b32553f744734fb99e79595ab12b8beb2b7e
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-07-29 13:03:00 +0100

    Included all completed components in main index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,32 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Navigation from '../src/components/Navigation';
-import Menu from '../src/components/Menu';
-
-import Testimonials from '../src/components/Testimonials';
-
-import WhyChooseUs from '../src/components/WhyChooseUs';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navigation from "../src/components/Navigation";
+import Header from "../src/components/Header";
+import SearchBar from "../src/components/SearchBar";
+import Testimonials from "../src/components/Testimonials";
+import WhyChooseUs from "../src/components/WhyChooseUs";
 import AboutUs from "../src/components/AboutUs";
 import Footer from "../src/components/Footer";
-
-
-
+import SkillGallery from "../src/components/SkillGallery";
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>SkillEx</title>
-				<meta name="description" content="SkillEx " />
+				<meta
+					name="SkillEx - The knowhow exchange where people swap skills."
+					content="SkillEx "
+				/>
 				<link rel="icon" href="/LightBulbFAVICON.ico" />
 			</Head>
-
 			<Navigation />
-			<Testimonials/>
-
+			<Header />
+			<SearchBar />
 			<AboutUs />
-      <WhyChooseUs/>
-<Footer/>
-			</div>
+			<WhyChooseUs />
+			<Testimonials />
+			<SkillGallery />
+			<Footer />
+		</div>
 	);
 }
