a599974bb0e9695e192066372a7937f2d8a21551
piratejas <piratejas@gmail.com>
2022-07-29 10:28:59 +0100

    created AboutUs component & css module

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,1 +1,22 @@
-//About
+import Image from "next/image";
+import styles from "./AboutUs.module.css";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h2 className="about-us-title">About Us</h2>
+      <img
+        src="/media/images/skilswapping.png"
+        alt="diagram showing how skillswapping works"
+        className="about-us-image"
+      />
+      <p className="about-us-text">
+        SkillEx is the place where exchanging skills and talents becomes easy.
+        Swap your skills, find what you are looking for It’s all free, you just
+        “pay” with your time.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
