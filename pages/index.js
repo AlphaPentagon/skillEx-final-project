d0aef4e3e2cd3b9030872e72b61ce919120e25f9
piratejas <piratejas@gmail.com>
2022-07-29 12:25:05 +0100

    footer and child divs created; css module

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,20 @@
-
-
-
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Navigation from '../src/components/Navigation';
-import Menu from '../src/components/Menu';
-
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navigation from "../src/components/Navigation";
+import Menu from "../src/components/Menu";
+import Footer from "../src/components/Footer";
 
 export default function Home() {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>SkillEx</title>
-				<meta name="description" content="SkillEx " />
-				<link rel="icon" href="/LightBulbFAVICON.ico" />
-			</Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>SkillEx</title>
+        <meta name="description" content="SkillEx " />
+        <link rel="icon" href="/LightBulbFAVICON.ico" />
+      </Head>
 
-      		
-			<Navigation />
-
-
-	
-
-
-		</div>
-	);
+      <Navigation />
+      <Footer />
+    </div>
+  );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,57 @@
-//Footer
+import styles from "./Footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.sitemapContainer}>
+        <Link href="/">
+          <a title="Go to home page">Home</a>
+        </Link>
+        <Link href="/">
+          <a title="Discover skills">Discover</a>
+        </Link>
+        <Link href="/">
+          <a title="Get in touch">Contact us</a>
+        </Link>
+      </div>
+      <div className={styles.otherLinksContainer}>
+        <Link href="/">
+          <a title="Code of Conduct">Code of Conduct</a>
+        </Link>
+        <Link href="/">
+          <a title="Privacy Policy">Privacy Policy</a>
+        </Link>
+        <Link href="/">
+          <a title="Terms of Service">Terms of Service</a>
+        </Link>
+      </div>
+      <div className={styles.socialContainer}>
+        <Link href="/">
+          <a title="Facebook">
+            <Image src="/media/icons/facebook.png" width={50} height={50} />
+          </a>
+        </Link>
+        <Link href="/">
+          <a title="Instagram">
+            <Image src="/media/icons/instagram.png" width={50} height={50} />
+          </a>
+        </Link>
+        <Link href="/">
+          <a title="TikTok">
+            <Image src="/media/icons/tiktok.png" width={50} height={50} />
+          </a>
+        </Link>
+        <Link href="/">
+          <a title="Twitter">
+            <Image src="/media/icons/twitter.png" width={50} height={50} />
+          </a>
+        </Link>
+      </div>
+      <div className={styles.copyrightContainer}>© 2022 SkillEx</div>
+    </div>
+  );
+};
+
+export default Footer;
