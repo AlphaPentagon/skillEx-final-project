66bd3f282acb88865d067744a00d6dbd05a1a200
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-01 10:14:54 +0100

    created pages, added layout globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../src/components/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
