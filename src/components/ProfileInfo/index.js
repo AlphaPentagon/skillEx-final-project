c07fa72602c8eeb5bf2208014f54562e6d3f9a6a
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-01 14:50:25 +0100

    styled profile info component. changed background for all pages using layout component.  Updated profiles data

--- src/components/ProfileInfo/index.js
+++ src/components/ProfileInfo/index.js
@@ -1,30 +1,50 @@
 import styles from "./profileInfo.module.css";
 import Button from "../Button/index";
+import { useState } from "react";
 
-const ProfileInfo = () => {
+const ProfileInfo = ({ profile }) => {
+  const [text, setText] = useState("");
+  const handleSend = (event) => {
+    event.preventDefault();
+    console.log(text);
+    setText("");
+  };
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
-    <section>
-      <div>
-        <h3>Description:</h3>
-        <p></p>
+    <section className={styles.profileSection}>
+      <div className={styles.profileInfoContainer}>
+        <h3>Bio</h3>
+        <p>{profile.bio}</p>
       </div>
-      <div>
-        <h3>Teach:</h3>
+      <div className={styles.profileInfoContainer}>
+        <h3>Teach</h3>
         <ul>
-          <li></li>
+          {profile.teach_skills.map((skill) => (
+            <li>{skill}</li>
+          ))}
         </ul>
       </div>
-      <div>
-        <h3>Learn:</h3>
+      <div className={styles.profileInfoContainer}>
+        <h3>Learn</h3>
         <ul>
-          <li></li>
+          {profile.learn_skills.map((skill) => (
+            <li>{skill}</li>
+          ))}
         </ul>
       </div>
-      <div>
-        <h3>Write a message:</h3>
-        <form>
-          <input></input>
-          <Button color="blue" text="Submit" />
+      <div className={styles.profileInfoContainer}>
+        <h3>Write a message</h3>
+        <form className={styles.profileInfoForm} onSubmit={handleSend}>
+          <textarea
+            onChange={handleChange}
+            placeholder="Type your message here..."
+            value={text}
+          ></textarea>
+          <Button colour="blue" text="Send" />
         </form>
       </div>
     </section>
