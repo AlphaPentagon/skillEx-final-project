0ab38a743bbedb1c6ba226bb900dd35969a79936
piratejas <47509562+piratejas@users.noreply.github.com>
2022-08-01 11:58:34 +0100

    created profileCard & gallery components; html tags; assigned class names; passed props; created css modules

--- src/components/ProfileCard/index.js
+++ src/components/ProfileCard/index.js
@@ -1,1 +1,33 @@
-//ProfileCard
+import styles from "./ProfileCard/ProfileCard.module.css";
+import Image from "next/image";
+import Link from "next/link";
+//import Avatar from "../Avatar/index.js";
+import Button from "../Button/index.js";
+
+const ProfileCard = ({
+  id,
+  username,
+  teachSkills,
+  learnSkills,
+  bio,
+  avatar_url,
+}) => {
+  return (
+    <Link href={`/profiles/${id}`}>
+      <div className={styles.ProfileCard}>
+        <Image
+          className={styles.image}
+          layout="fill"
+          src={avatar_url}
+          alt="profile picture"
+        />
+        <div className={styles.username}>{username}</div>
+        <div className={styles.skills}>{teachSkills}</div>
+        <p className={styles.bio}>{bio}</p>
+        <Button></Button>
+      </div>
+    </Link>
+  );
+};
+
+export default ProfileCard;
