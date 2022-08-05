61f16198b2ee18309cadfe2040ac0dd9c364d72b
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-05 14:41:55 +0100

    Added fetch request for discover page, profile gallery now renders fetched profile object, changed sign-up form button back to regular html butto

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -1,18 +1,31 @@
-
 import Header from "../../src/components/Header";
 import ProfileGallery from "../../src/components/ProfileGallery";
 import Searchbar from "../../src/components/SearchBar";
 import SkillGallery from "../../src/components/SkillGallery/index";
 import styles from "./discover.module.css";
-const Discover = () => {
-	return (
-		<>
-			<Header text="Discover" />
-			<ProfileGallery />
-			<Searchbar />
-			<SkillGallery />
-		</>
-	);
+
+// fetch all profiles
+// pass the array to the SkillGallery component
+
+const Discover = ({ profilesArr }) => {
+  console.log(profilesArr);
+  return (
+    <>
+      <Header text="Discover" />
+      <ProfileGallery profiles={profilesArr} />
+      <Searchbar />
+      <SkillGallery />
+    </>
+  );
 };
 
 export default Discover;
+
+// needed for data fetching before rendering - makes the profileId available as a prop in the component
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/profiles");
+  const profilesArr = await res.json();
+  return {
+    props: { profilesArr },
+  };
+}
