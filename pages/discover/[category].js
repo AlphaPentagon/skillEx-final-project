3a744f96cc04cb0ba9cc74e9d9680339fed7400d
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-08-05 14:13:14 +0100

    Fixed wonky skill label text alighment on landing page

--- pages/discover/[category].js
+++ pages/discover/[category].js
@@ -3,7 +3,6 @@ import Header from "../../src/components/Header";
 import ProfileGallery from "../../src/components/ProfileGallery";
 import Searchbar from "../../src/components/SearchBar";
 import SkillGallery from "../../src/components/SkillGallery/index";
-import styles from "./discover.module.css";
 const Discover = () => {
 	const router = useRouter();
 	const { category } = router.query;
