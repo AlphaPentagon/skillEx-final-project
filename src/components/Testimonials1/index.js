e4dd72e831737c922d75ded96f863698a6fc4bd4
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-08-05 09:43:35 +0100

    Included my testimonial. Added speech marks and punctuation to existing testimonials

--- src/components/Testimonials1/index.js
+++ src/components/Testimonials1/index.js
@@ -22,8 +22,8 @@ export default function SimpleSlider() {
 				<div className={styles.allTestimonials}>
 					<div className={styles.containerTestimonials}>
 						<p className={styles.testimonials}>
-							This site is amazing and has really helped me to improve my
-							skills
+							"This site is amazing and has really helped me to improve
+							my skills!""
 						</p>
 						<p className={styles.title}>Lucy de Rojas</p>
 					</div>
@@ -31,21 +31,32 @@ export default function SimpleSlider() {
 
 				<div className={styles.containerTestimonials}>
 					<p className={styles.testimonials}>
-						Fantastic tutors, all willing to help me to learn piano
+						"Fantastic tutors, all willing to help me to learn piano.""
 					</p>
 					<p className={styles.title}>Craig Summers</p>
 				</div>
 
 				<div className={styles.containerTestimonials}>
 					<p className={styles.testimonials}>
-						Love this site, I teach and learn which is great
+						"Love this site, I teach and learn which is great.""
 					</p>
 					<p className={styles.title}>Zainab Abid</p>
 				</div>
 
 				<div className={styles.containerTestimonials}>
 					<p className={styles.testimonials}>
-					"Got fed up with little things around the house breaking and having to pay an arm and a leg to get a contractor in to fix them. Popped over to SkillEx and quickly learnt how to fix all of my own DIY problems! What a site!"
+						"A brilliant resource for upskilling! The community is
+						wonderful. I love the concept and ethos."
+					</p>
+					<p className={styles.title}>Dave Hazeldean</p>
+				</div>
+
+				<div className={styles.containerTestimonials}>
+					<p className={styles.testimonials}>
+						"Got fed up with little things around the house breaking and
+						having to pay an arm and a leg to get a contractor in to fix
+						them. Popped over to SkillEx and quickly learnt how to fix all
+						of my own DIY problems! What a site!"
 					</p>
 					<p className={styles.title}>Jay Whittingham</p>
 				</div>
