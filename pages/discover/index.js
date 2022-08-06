50cdd32554533cb15fae95aa61844408da64f28c
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-06 22:56:36 +0100

    changed logic on discover page so data fetched directly from db - same reason as previous commit Also removed two files - an api test page and an auth0 test page, as these were causing issues at build time

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -3,6 +3,7 @@ import ProfileGallery from "../../src/components/ProfileGallery";
 import Searchbar from "../../src/components/SearchBar";
 import SkillGallery from "../../src/components/SkillGallery/index";
 import styles from "./discover.module.css";
+import prisma from "../../prisma/client";
 
 // fetch all profiles
 // pass the array to the SkillGallery component
@@ -23,9 +24,8 @@ export default Discover;
 
 // needed for data fetching before rendering - makes the profileId available as a prop in the component
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/api/profiles");
-  const profilesArr = await res.json();
+  const data = await prisma.profiles.findMany();
   return {
-    props: { profilesArr },
+    props: { profilesArr: data },
   };
 }
