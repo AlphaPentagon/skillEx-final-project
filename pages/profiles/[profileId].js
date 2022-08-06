a631e12f838afad196db7c0017a83cff6b4e37fc
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-06 22:48:08 +0100

    Changed profileId page so data is requested directly from the db using prisma, and not the api route this is because the api route cannot be reached during build, so the db has to be queried directly

--- pages/profiles/[profileId].js
+++ pages/profiles/[profileId].js
@@ -1,16 +1,11 @@
-// import { useRouter } from "next/router";
+import prisma from "../../prisma/client";
 import ProfileInfo from "../../src/components/ProfileInfo";
-// import profiles from "../../src/libs/profiles";
 import Header from "../../src/components/Header/index";
 import Avatar from "../../src/components/Avatar/index";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default withPageAuthRequired(function Profile({ profile }) {
   console.log("current profile: ", profile);
-  // const router = useRouter();
-  // const { profileId } = router.query;
-  // const CURRENT_INDEX = profileId - 1;
-  // const CURRENT_PROFILE = profiles[CURRENT_INDEX];
 
   return (
     <>
@@ -23,8 +18,7 @@ export default withPageAuthRequired(function Profile({ profile }) {
 
 // needed for data fetching before rendering - creates static versions of each page/path
 export async function getStaticPaths() {
-  const response = await fetch(`http://localhost:3000/api/profiles`);
-  const data = await response.json();
+  const data = await prisma.profiles.findMany();
 
   const paths = data.map((profile) => {
     return {
@@ -44,11 +38,10 @@ export async function getStaticPaths() {
 // needed for data fetching before rendering - makes the profileId available as a prop in the component
 export async function getStaticProps(context) {
   const { params } = context;
-  const response = await fetch(
-    `http://localhost:3000/api/profiles/${params.profileId}`
-  );
-  console.log(params.profileId);
-  const data = await response.json();
+  const data = await prisma.profiles.findUnique({
+    where: { id: Number(params.profileId) },
+  });
+
   console.log("profile: ", data);
   return {
     props: { profile: { ...data } },
