324dc8e32e0801a3809e69c1f60f01a459c55c16
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-08 13:56:40 +0100

    fixed data fetching by switching to server side props

--- pages/discover/[category].js
+++ pages/discover/[category].js
@@ -30,42 +30,67 @@ export default Discover;
   queries the database directly using prisma as an ORM and tells next what profile pages to create, based on the id
   do NOT use fetch requests, as explained in this article here - https://stackoverflow.com/questions/61452675/econnrefused-during-next-build-works-fine-with-next-dev */
 
-export async function getStaticPaths() {
-  // skills categories currently hardcoded - eventually this will be a table in our db and we will need to query the db to get this data
-  const data = [
-    "accounting",
-    "cooking",
-    "diy",
-    "english",
-    "languages",
-    "business",
-    "gardening",
-    "construction",
-    "craft",
-    "it",
-    "martial+arts",
-  ];
+// export async function getStaticPaths() {
+//   // skills categories currently hardcoded - eventually this will be a table in our db and we will need to query the db to get this data
+//   const data = [
+//     "accounting",
+//     "cooking",
+//     "diy",
+//     "english",
+//     "languages",
+//     "business",
+//     "gardening",
+//     "construction",
+//     "craft",
+//     "it",
+//     "martial+arts",
+//   ];
 
-  const paths = data.map((category) => {
-    return {
-      params: {
-        category,
-      },
-    };
-  });
-  console.log(paths);
+//   const paths = data.map((category) => {
+//     return {
+//       params: {
+//         category,
+//       },
+//     };
+//   });
+//   console.log(paths);
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-/* needed for data fetching before rendering - makes the relevant profiles available as a prop in the component
-	queries the database directly using prisma as an ORM and fetches the current profile, based on the id
-	do NOT use fetch requests, as explained in this article here - https://stackoverflow.com/questions/61452675/econnrefused-during-next-build-works-fine-with-next-dev */
+// /* needed for data fetching before rendering - makes the relevant profiles available as a prop in the component
+// 	queries the database directly using prisma as an ORM and fetches the current profile, based on the id
+// 	do NOT use fetch requests, as explained in this article here - https://stackoverflow.com/questions/61452675/econnrefused-during-next-build-works-fine-with-next-dev */
 
-export async function getStaticProps(context) {
+// export async function getStaticProps(context) {
+//   const { params } = context;
+//   const data = await prisma.profiles.findMany({
+//     where: {
+//       OR: [
+//         {
+//           learn_skills: {
+//             has: params.category,
+//           },
+//         },
+//         {
+//           teach_skills: {
+//             has: params.category,
+//           },
+//         },
+//       ],
+//     },
+//   });
+
+//   console.log(params.category + " profiles: ", data);
+//   return {
+//     props: { profiles: [...data], category: params.category },
+//   };
+// }
+
+export async function getServerSideProps(context) {
   const { params } = context;
   const data = await prisma.profiles.findMany({
     where: {
