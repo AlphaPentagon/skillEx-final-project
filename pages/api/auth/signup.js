f6256e2de2ecc47497086324a7d07e239f162547
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-08 12:18:49 +0100

    changed url in api signup to relative

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,18 +2,18 @@
 import { handleLogin } from "@auth0/nextjs-auth0";
 
 export default async function signup(req, res) {
-	try {
-		await handleLogin(req, res, {
-			// TODO: Remove hardcoded localhost address
-			returnTo: "http://localhost:3000/profiles/sign-up",
-			authorizationParams: {
-				// Note that this can be combined with prompt=login , which indicates if
-				// you want to always show the authentication page or you want to skip
-				// if there’s an existing session.
-				screen_hint: "signup",
-			},
-		});
-	} catch (error) {
-		res.status(error.status || 400).end(error.message);
-	}
+  try {
+    await handleLogin(req, res, {
+      // TODO: Remove hardcoded localhost address
+      returnTo: "/profiles/sign-up",
+      authorizationParams: {
+        // Note that this can be combined with prompt=login , which indicates if
+        // you want to always show the authentication page or you want to skip
+        // if there’s an existing session.
+        screen_hint: "signup",
+      },
+    });
+  } catch (error) {
+    res.status(error.status || 400).end(error.message);
+  }
 }
