b90887422dd21dd7bb6ed17eb80d4c23d102fe9e
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-08-11 10:24:33 +0100

    Discover index.js unused lines commented out

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -2,32 +2,30 @@ import Header from "../../src/components/Header";
 import ProfileGallery from "../../src/components/ProfileGallery";
 import Searchbar from "../../src/components/SearchBar";
 import SkillGallery from "../../src/components/SkillGallery/index";
-import styles from "./discover.module.css";
+//import styles from "./discover.module.css";
 import prisma from "../../prisma/client";
-import stylesHome from "../../styles/Home.module.css"
+import stylesHome from "../../styles/Home.module.css";
 import Head from "next/head";
-import Link from "next/link";
-import Button from "../../src/components/Button";
-
+//import Link from "next/link";
+//import Button from "../../src/components/Button";
 
 const Discover = ({ profilesArr }) => {
-  console.log(profilesArr);
-  return (
-    <>
-    <Head>
-      <title>Discover</title>
-    </Head>
-      <Header text="Discover" />
-      <p className={stylesHome.sloganText}>
-        Teach and Learn at the same time, all you need to search and find all
-        the tutors that teach that skill
-      </p>
-      <ProfileGallery profiles={profilesArr} />
-    
-      <SkillGallery />
+	console.log(profilesArr);
+	return (
+		<>
+			<Head>
+				<title>Discover</title>
+			</Head>
+			<Header text="Discover" />
+			<p className={stylesHome.sloganText}>
+				Teach and Learn at the same time, all you need to search and find
+				all the tutors that teach that skill
+			</p>
+			<ProfileGallery profiles={profilesArr} />
 
-    </>
-  );
+			<SkillGallery />
+		</>
+	);
 };
 
 export default Discover;
@@ -48,8 +46,8 @@ export default Discover;
   do NOT use fetch requests, as explained in this article here - https://stackoverflow.com/questions/61452675/econnrefused-during-next-build-works-fine-with-next-dev */
 
 export async function getServerSideProps() {
-  const data = await prisma.profiles.findMany();
-  return {
-    props: { profilesArr: data },
-  };
+	const data = await prisma.profiles.findMany();
+	return {
+		props: { profilesArr: data },
+	};
 }
