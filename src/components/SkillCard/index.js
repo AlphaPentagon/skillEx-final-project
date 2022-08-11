3ce0c51f3af1e89433bdb03b3ac5174f5a531d4c
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-08-11 13:52:31 +0100

    Added ID to menu

--- src/components/SkillCard/index.js
+++ src/components/SkillCard/index.js
@@ -3,23 +3,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 const SkillCard = ({ image, title, description }) => {
-  return (
-    <div className={styles.skillCard}>
-      <Link href={"/discover/" + title}>
-        <a aria-label={title}>
-          <Image
-            className={styles.image}
-            src={image}
-            alt={description}
-            layout="fill"
-          />
-        </a>
-      </Link>
-      <a className={styles.text} href={"/discover/" + title} aria-label={title}>
-        {title}
-      </a>
-    </div>
-  );
+	return (
+		<div className={styles.skillCard} id={"skill-card-" + title}>
+			<Link href={"/discover/" + title}>
+				<a aria-label={title}>
+					<Image
+						className={styles.image}
+						src={image}
+						alt={description}
+						layout="fill"
+					/>
+				</a>
+			</Link>
+			<a
+				className={styles.text}
+				href={"/discover/" + title}
+				aria-label={title}
+			>
+				{title}
+			</a>
+		</div>
+	);
 };
 
 export default SkillCard;
