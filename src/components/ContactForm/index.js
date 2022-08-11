ab03e772009308fc34cc420a7fe023272c491bea
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-08-11 17:17:04 +0100

    Remaining console logs removed from code

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -5,72 +5,72 @@ import styles from "./contactform.module.css";
 import { useState } from "react";
 
 const ContactForm = () => {
-  const [text, setText] = useState("");
-  const [messageSent, setMessageSent] = useState(false);
+	const [text, setText] = useState("");
+	const [messageSent, setMessageSent] = useState(false);
 
-  const handleSend = (event) => {
-    event.preventDefault();
-    console.log(text);
-    setText("");
-    setMessageSent(true);
-    console.log(messageSent);
-  };
+	const handleSend = (event) => {
+		event.preventDefault();
+		//console.log(text);
+		setText("");
+		setMessageSent(true);
+		//console.log(messageSent);
+	};
 
-  const handleChange = (event) => {
-    setText(event.target.value);
-  };
+	const handleChange = (event) => {
+		setText(event.target.value);
+	};
 
-  return (
-    <>
-      <title>Contact us</title>
-      <Header text="Contact us" colour="terraCotta" />
-      <form className={styles.form} onSubmit={handleSend}>
-        <div className={styles.inputContainer}>
-          <div className={styles.padding}>
-            <label className={styles.label} />
-            Email:
-          </div>
-          <input
-            className={styles.emailInput}
-            type="text"
-            name="email"
-            placeholder=""
-          />
-        </div>
-        <br></br>
-        <div className={styles.inputContainer}>
-          <div className={styles.padding}>
-            <label className={styles.label} />
-            Message:
-          </div>
+	return (
+		<>
+			<title>Contact us</title>
+			<Header text="Contact us" colour="terraCotta" />
+			<form className={styles.form} onSubmit={handleSend}>
+				<div className={styles.inputContainer}>
+					<div className={styles.padding}>
+						<label className={styles.label} />
+						Email:
+					</div>
+					<input
+						className={styles.emailInput}
+						type="text"
+						name="email"
+						placeholder=""
+					/>
+				</div>
+				<br></br>
+				<div className={styles.inputContainer}>
+					<div className={styles.padding}>
+						<label className={styles.label} />
+						Message:
+					</div>
 
-          <textarea
-            className={styles.messageInput}
-            type="text"
-            name="message"
-            placeholder=""
-            onChange={handleChange}
-            value={text}
-          ></textarea>
-        </div>
-        <br></br>
-        <div className={styles.contactButton}>
-          <Button type="contactFormButton" text="Submit" />
-        </div>
-      </form>
-      <h3>{messageSent ? "Thank you for your message!" : null}</h3>
-      <div className={styles.contactInfo}>
-        <p className={styles.label}>Address:</p>
-        <p>
-          2 Whitehall Ct, <br></br>
-          London <br></br>
-          SW1A 2EJ
-        </p>
-        <p className={styles.label}>Phone: </p>
-        <p>020 008 87545</p>
-      </div>
-    </>
-  );
+					<textarea
+						className={styles.messageInput}
+						type="text"
+						name="message"
+						placeholder=""
+						onChange={handleChange}
+						value={text}
+					></textarea>
+				</div>
+				<br></br>
+				<div className={styles.contactButton}>
+					<Button type="contactFormButton" text="Submit" />
+				</div>
+			</form>
+			<h3>{messageSent ? "Thank you for your message!" : null}</h3>
+			<div className={styles.contactInfo}>
+				<p className={styles.label}>Address:</p>
+				<p>
+					2 Whitehall Ct, <br></br>
+					London <br></br>
+					SW1A 2EJ
+				</p>
+				<p className={styles.label}>Phone: </p>
+				<p>020 008 87545</p>
+			</div>
+		</>
+	);
 };
 
 export default ContactForm;
--- src/components/ProfileMessage/index.js
+++ src/components/ProfileMessage/index.js
@@ -3,34 +3,34 @@ import Button from "../Button";
 import { useState } from "react";
 
 const ProfileMessage = ({ profile }) => {
-  const [text, setText] = useState("");
-  const [messageSent, setMessageSent] = useState(false);
+	const [text, setText] = useState("");
+	const [messageSent, setMessageSent] = useState(false);
 
-  const handleSend = (event) => {
-    event.preventDefault();
-    console.log(text);
-    setText("");
-    setMessageSent(true);
-    console.log(messageSent);
-  };
+	const handleSend = (event) => {
+		event.preventDefault();
+		//console.log(text);
+		setText("");
+		setMessageSent(true);
+		//console.log(messageSent);
+	};
 
-  const handleChange = (event) => {
-    setText(event.target.value);
-  };
+	const handleChange = (event) => {
+		setText(event.target.value);
+	};
 
-  return (
-    <div className={styles.messageContainer}>
-      <form className={styles.profileInfoForm} onSubmit={handleSend}>
-        <textarea
-          onChange={handleChange}
-          placeholder="Type your message here..."
-          value={text}
-        ></textarea>
-        <Button type="profileInfoButton" text="Send" />
-      </form>
-      <h3>{messageSent ? "Your message has been sent!" : null}</h3>
-    </div>
-  );
+	return (
+		<div className={styles.messageContainer}>
+			<form className={styles.profileInfoForm} onSubmit={handleSend}>
+				<textarea
+					onChange={handleChange}
+					placeholder="Type your message here..."
+					value={text}
+				></textarea>
+				<Button type="profileInfoButton" text="Send" />
+			</form>
+			<h3>{messageSent ? "Your message has been sent!" : null}</h3>
+		</div>
+	);
 };
 
 export default ProfileMessage;
