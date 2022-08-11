f96a90d45ceb49af495fe5f514f4a8dea816f4c0
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-11 14:00:50 +0100

    Readded nav bar changes

--- src/components/desktopNavigation/desktopNavigation.js
+++ src/components/desktopNavigation/desktopNavigation.js
@@ -4,67 +4,68 @@ import styles from "./desktopNavBar.module.css";
 export default function DesktopNavBar() {
   const { user } = useUser();
   return (
-
-    <div className={styles.navigation}>
-      <div className={styles.navigationMenu}>
-        <ul className={styles.navBar}>
+    <nav className={styles.navigationMenu}>
+      <ul className={styles.navBar}>
+        <li>
           <Link href="/">
-            <a className={styles.word} title="Go to home page">
+            <a className={styles.link} title="Go to home page">
               Home
             </a>
           </Link>
+        </li>
+        <li>
           <Link href="/discover">
-            <a className={styles.word} title="Discover skills">
+            <a className={styles.link} title="Discover skills">
               Discover
             </a>
           </Link>
+        </li>
+
+        <li>
           <Link href="/contact">
-            <a className={styles.word} title="Get in touch">
+            <a className={styles.link} title="Get in touch">
               Contact
             </a>
           </Link>
+        </li>
 
-          <br />
-
-          <ul className={styles.buttons}>
-            {!user && (
-              <>
-                <a
-                  href="/api/auth/signup"
-                  className={styles.button}
-                  title="Sign up"
-                >
+        {!user && (
+          <>
+            <li>
+              <Link href="/api/auth/signup">
+                <a className={styles.button} title="Sign up">
                   SignUp
                 </a>
-                <a
-                  href="/api/auth/login"
-                  className={styles.button}
-                  title="Log in"
-                >
+              </Link>
+            </li>
+            <li>
+              <Link href="/api/auth/login">
+                <a className={styles.button} title="Log in">
                   Login
                 </a>
-              </>
-            )}
-            {user && (
-              <>
-                <Link href="/profiles/my-profile">
-                  <a className={styles.word} title="View your profile">
-                    Profile
-                  </a>
-                </Link>
-
-                <a
-                  className={styles.button}
-                  href="/api/auth/logout"
-                  title="Log out of your profile"
-                >
+              </Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              <Link href="/profiles/my-profile">
+                <a className={styles.link} title="View your profile">
+                  Profile
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/api/auth/logout">
+                <a className={styles.button} title="Log out of your profile">
                   Log-Out
                 </a>
-              </>
-            )}
-          </ul>
-        </ul>
-      </div>
-    </div>
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
   );
 }
