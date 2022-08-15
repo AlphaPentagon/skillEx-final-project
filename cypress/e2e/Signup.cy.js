8dde20591a927795bc1796f683a291b9d3671344
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-08-15 09:34:00 +0100

    Profile avatar and greeting included in mobile view. Cypress E2E tests.

--- cypress/e2e/Signup.cy.js
+++ cypress/e2e/Signup.cy.js
@@ -2,14 +2,13 @@ describe("Sign up E2E", () => {
 	it("Signs up", () => {
 		cy.visit("https://skill-ex.vercel.app");
 		cy.get(".menu_menu__1P4Y_").click();
-		cy.get('[href="/api/auth/signup"]').click();
-
+		cy.get('.menu_userLinks__ZwXtF > [href="/api/auth/signup"]').click();
 		cy.origin("https://skillex.eu.auth0.com", () => {
 			cy.get("#email").type(Cypress.env("SIGNUPEMAIL"));
 			cy.get("#password").type(Cypress.env("PASSWORD"));
 			cy.get("#btn-signup").click();
 		});
 
-		cy.get("h1").should("contain", "Sign Up Form");
+		cy.get("h1").should("contain", "Sign Up");
 	});
 });
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import styles from "./menu.module.css";
 import { useUser } from "@auth0/nextjs-auth0";
-import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 export default function Menu() {
 	//Menu state
 	const [menuOpen, setMenuOpen] = useState(false);
@@ -34,111 +33,113 @@ export default function Menu() {
 
 	return (
 		<>
-			{!menuOpen && !user && (
-				<div className={styles.menu} onClick={toggleMenu}>
-					<Image
-						className={styles.menuOpenImage}
-						src="/media/icons/menuIcon.png"
-						alt="open menu icon"
-						width={90}
-						height={90}
-					/>
-				</div>
-			)}
-			{!menuOpen && user && (
-				<div className={styles.menu} onClick={toggleMenu}>
-					<Image
-						className={styles.menuProfileImage}
-						src={currentProfile.avatar_url}
-						alt="open menu icon"
-						width={90}
-						height={90}
-					/>
-				</div>
-			)}
-			{menuOpen && (
-				<div className={styles.modal} onClick={toggleMenu}>
-					<div className={styles.menuClose}>
+			<div id="menu">
+				{!menuOpen && !user && (
+					<div className={styles.menu} onClick={toggleMenu}>
 						<Image
-							className={styles.menuCloseImage}
-							src="/media/icons/menuClose.png"
-							alt="close menu icon"
-							width={60}
-							height={60}
+							className={styles.menuOpenImage}
+							src="/media/icons/menuIcon.png"
+							alt="open menu icon"
+							width={90}
+							height={90}
 						/>
 					</div>
-					{user && (
-						<>
-							<p className={styles.pageLinks}>
-								Hi, {currentProfile.preferred_name}!
-							</p>
-							<img
-								className={styles.menuProfileImage}
-								src={currentProfile.avatar_url}
-								alt={currentProfile.preferred_name}
-								height={50}
-								width={50}
+				)}
+				{!menuOpen && user && (
+					<div className={styles.menu} onClick={toggleMenu}>
+						<Image
+							className={styles.menuProfileImage}
+							src={currentProfile.avatar_url}
+							alt="open menu icon"
+							width={90}
+							height={90}
+						/>
+					</div>
+				)}
+				{menuOpen && (
+					<div className={styles.modal} onClick={toggleMenu}>
+						<div className={styles.menuClose}>
+							<Image
+								className={styles.menuCloseImage}
+								src="/media/icons/menuClose.png"
+								alt="close menu icon"
+								width={60}
+								height={60}
 							/>
-						</>
-					)}
-
-					<ul className={styles.pageLinks}>
-						<Link href="/">
-							<a title="Go to home page">Home</a>
-						</Link>
-						<Link href="/discover">
-							<a title="Discover skills">Discover</a>
-						</Link>
-						<Link href="/contact">
-							<a title="Get in touch">Contact us</a>
-						</Link>
-					</ul>
-
-					<br />
-
-					<ul className={styles.userLinks}>
-						{!user && (
-							<>
-								<a
-									href="/api/auth/signup"
-									title="Sign up"
-									onClick={() => {
-										toggleMenu();
-									}}
-								>
-									Sign up
-								</a>
-								<a
-									href="/api/auth/login"
-									title="Log in"
-									onClick={() => {
-										toggleMenu();
-									}}
-								>
-									Login
-								</a>
-							</>
-						)}
+						</div>
 						{user && (
 							<>
-								<Link href="/profiles/my-profile">
-									<a title="View your profile">My profile</a>
-								</Link>
-
-								<a
-									href="/api/auth/logout"
-									title="Log out of your profile"
-									onClick={() => {
-										toggleMenu();
-									}}
-								>
-									Log out
-								</a>
+								<p className={styles.pageLinks}>
+									Hi, {currentProfile.preferred_name}!
+								</p>
+								<img
+									className={styles.menuProfileImage}
+									src={currentProfile.avatar_url}
+									alt={currentProfile.preferred_name}
+									height={90}
+									width={90}
+								/>
 							</>
 						)}
-					</ul>
-				</div>
-			)}
+
+						<ul className={styles.pageLinks}>
+							<Link href="/">
+								<a title="Go to home page">Home</a>
+							</Link>
+							<Link href="/discover">
+								<a title="Discover skills">Discover</a>
+							</Link>
+							<Link href="/contact">
+								<a title="Get in touch">Contact us</a>
+							</Link>
+						</ul>
+
+						<br />
+
+						<ul className={styles.userLinks}>
+							{!user && (
+								<>
+									<a
+										href="/api/auth/signup"
+										title="Sign up"
+										onClick={() => {
+											toggleMenu();
+										}}
+									>
+										Sign up
+									</a>
+									<a
+										href="/api/auth/login"
+										title="Log in"
+										onClick={() => {
+											toggleMenu();
+										}}
+									>
+										Login
+									</a>
+								</>
+							)}
+							{user && (
+								<>
+									<Link href="/profiles/my-profile">
+										<a title="View your profile">My profile</a>
+									</Link>
+
+									<a
+										href="/api/auth/logout"
+										title="Log out of your profile"
+										onClick={() => {
+											toggleMenu();
+										}}
+									>
+										Log out
+									</a>
+								</>
+							)}
+						</ul>
+					</div>
+				)}
+			</div>
 		</>
 	);
 }
